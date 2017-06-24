fac9e94fc3044200d02ca907432f929731ff5a8f
dprice06468 <thepricefamily@gmail.com>
2017-06-24 09:32:56 -0400

    initial checkin

---
+++ basic/basicFrogServoIR/basicFrogServoIR.ino
@@ -0,0 +1,83 @@
+#include <IRremote.h>
+#include <Servo.h>
+
+//set Frog servo pin
+#define FROG_SERVO_PIN 13
+
+Servo frogServo;
+
+#define LEDPIN 1
+#define IRRECVPIN 3 //PIN receiving signal from IR (moteino 9, mega 3)
+#define IR_ENTER 0x219E10EF
+
+IRrecv irrecv(IRRECVPIN);
+decode_results results;
+
+//----------------------------------------------------------------------
+//numvar checkIR(void) {
+//----------------------------------------------------------------------
+int checkir() {
+  Serial.println("basicIR.checkir");
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+      irrecv.resume(); 
+    if (results.value == IR_ENTER) {
+//      irrecv.resume(); 
+      return 1;
+    }
+    // Receive the next value
+//    irrecv.resume(); 
+  }
+  return 0;
+}
+
+//-------------------------------------------------------------------
+//numvar turnquick(void) {
+//  frogServo.writeMicroseconds(getarg(1));
+//  return getarg(1);
+//}
+//-------------------------------------------------------------------
+int turnquick(int ms) {
+  Serial.println("basicFrogServo.turnquick");
+  frogServo.writeMicroseconds(ms);
+  return ms;
+}
+
+//-------------------------------------------------------------------
+// this function will be implemented in bitlash as candystore
+// proximity action
+//-------------------------------------------------------------------
+void froghop() {
+  Serial.println("basicFrogServo.froghop");
+  frogServo.attach(FROG_SERVO_PIN);
+  turnquick(1200); 
+  delay(200); 
+  turnquick(1700);
+  frogServo.detach();
+}
+
+//-------------------------------------------------------------------
+void setup() {
+  Serial.begin(9600);
+  Serial.println("basicFrogServo.setup");
+
+  frogServo.attach(FROG_SERVO_PIN);
+  frogServo.writeMicroseconds(1500);
+  frogServo.detach();
+
+  irrecv.enableIRIn();
+  
+  // initialize output pins.
+  pinMode(LEDPIN, OUTPUT);
+}
+
+//-------------------------------------------------------------------
+void loop() {
+  if (checkir()) {
+    digitalWrite(LEDPIN, HIGH);
+    delay(2000);
+    digitalWrite(LEDPIN, LOW);
+  }
+  froghop();
+  delay(2000);
+}
---
+++ basic/basicIRServo/basicIRServo.ino
@@ -0,0 +1,63 @@
+// Written by: Mohamed Soliman
+// This code is for controlling servo motor with IR remote control
+// When clicking at any of two buttons the motor is toggling between the rotation and stop
+
+//#define __AVR_ATmega1280__
+#include <IRremote.h>      //must copy IRremote library to arduino libraries
+#include <Servo.h>
+#define plus 0x219E906F   //clockwise rotation button
+#define minus 0x219EE01F  //counter clockwise rotation button
+
+int RECV_PIN = 3;       //IR receiver pin
+Servo servo;
+int val;                //rotation angle
+bool cwRotation, ccwRotation;  //the states of rotation
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+#ifdef __AVR_ATmega1280__
+Serial.println("__AVR_ATmega1280__");
+#endif
+
+  irrecv.enableIRIn(); // Start the receiver
+  servo.attach(13);     //servo pin
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+
+    if (results.value == plus)
+    {
+      cwRotation = !cwRotation;      //toggle the rotation value
+      ccwRotation = false;         //no rotation in this direction
+    }
+
+    if (results.value == minus)
+    {
+      ccwRotation = !ccwRotation;   //toggle the rotation value
+      cwRotation = false;            //no rotation in this direction
+    }
+  }
+  if (cwRotation && (val != 175))  {
+    val++;                         //for colockwise button
+  }
+  if (ccwRotation && (val != 0))  {
+    val--;                         //for counter colockwise button
+  }
+//  servo.attach(13); 
+//  delay(100);
+  servo.write(val);
+//  delay(100);
+//  servo.detach();     
+//  delay(100);
+  delay(20);          //General speed
+}
+
